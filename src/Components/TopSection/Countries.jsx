86a706f27ad54eac54120d1ea0f476bd16b00c0d
MedEBoudali <med.sneaky@gmail.com>
2020-04-14 19:30:33 +0100

    Add react-select

--- src/Components/TopSection/Countries.jsx
+++ src/Components/TopSection/Countries.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import AsyncSelect from 'react-select/async';
 
 const Countries = () => {
-  return <div></div>;
+  const [, setInputValue] = useState('');
+
+  const colourOptions = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ];
+
+  const filterColors = inputValue => {
+    return colourOptions.filter(i =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+
+  const loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+      callback(filterColors(inputValue));
+    }, 1000);
+  };
+
+  const handleInputChange = newValue => {
+    const inputValue = newValue.replace(/\W/g, '');
+    setInputValue({ inputValue });
+    return inputValue;
+  };
+
+  return (
+    <div className='countriesSection'>
+      <div className='countriesDopdown'>
+        <AsyncSelect
+          placeholder="Search or Select Countrie ..."
+          cacheOptions
+          loadOptions={loadOptions}
+          defaultOptions
+          onInputChange={handleInputChange}
+          className='searchBox'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
