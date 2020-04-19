90dd5fe93b05beedfd35b25b65da4f63b8a4f46a
MedEBoudali <med.sneaky@gmail.com>
2020-04-19 03:06:01 +0100

    build for github pages

---
+++ build/precache-manifest.a77b5906aa0d08e91cfdb1e3a79bf70e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7656a2da7f23873ac05710fa563401a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f03a033b30c3608c8b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bc3ad538dcc8e3cf0552",
+    "url": "/static/css/main.75a52197.chunk.css"
+  },
+  {
+    "revision": "e4f03a033b30c3608c8b",
+    "url": "/static/js/2.8bc13993.chunk.js"
+  },
+  {
+    "revision": "6dcd373a94606ea12253d28720535d4c",
+    "url": "/static/js/2.8bc13993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3ad538dcc8e3cf0552",
+    "url": "/static/js/main.3f68f1ad.chunk.js"
+  },
+  {
+    "revision": "ad5a961890865248a731",
+    "url": "/static/js/runtime-main.c01df263.js"
+  }
+]);
