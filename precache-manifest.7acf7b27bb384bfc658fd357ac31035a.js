2c3aea875dbd5b252effb42ac8f31ebcaca60c1c
MedEBoudali <med.sneaky@gmail.com>
2020-04-19 03:19:20 +0100

    Updates

---
+++ precache-manifest.7acf7b27bb384bfc658fd357ac31035a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731b16fc72620c902afa09a3b1bbc263",
+    "url": "/Coronavirus-Update/index.html"
+  },
+  {
+    "revision": "d6fd8049dc1a9c01b8ba",
+    "url": "/Coronavirus-Update/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bc3ad538dcc8e3cf0552",
+    "url": "/Coronavirus-Update/static/css/main.75a52197.chunk.css"
+  },
+  {
+    "revision": "d6fd8049dc1a9c01b8ba",
+    "url": "/Coronavirus-Update/static/js/2.000771c0.chunk.js"
+  },
+  {
+    "revision": "6dcd373a94606ea12253d28720535d4c",
+    "url": "/Coronavirus-Update/static/js/2.000771c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3ad538dcc8e3cf0552",
+    "url": "/Coronavirus-Update/static/js/main.3f68f1ad.chunk.js"
+  },
+  {
+    "revision": "ea698e56c967cc46a484",
+    "url": "/Coronavirus-Update/static/js/runtime-main.f55a2789.js"
+  }
+]);
