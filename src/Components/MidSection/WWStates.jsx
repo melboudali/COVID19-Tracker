2a7e4b0befe4f49fe386a68f10d981045ce2ca06
MedEBoudali <med.sneaky@gmail.com>
2020-04-20 23:49:44 +0100

    Add hardcode data to WWStates

--- src/Components/MidSection/WWStates.jsx
+++ src/Components/MidSection/WWStates.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from 'react';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 const WWStates = () => {
-    return (
-        <div className='WWStates'>
-            
-        </div>
-    )
-}
+  return (
+    <div className='WWStates'>
+      <h1 className='WWStatesHeader'>
+        <i class='fas fa-globe'></i> Coronavirus World States
+      </h1>
+      <div className='WCDR'>
+        <OverlayTrigger
+          key='Cases'
+          placement='bottom'
+          overlay={
+            <Tooltip className='myToolTip' id='tooltip-bottom'>
+              Confirmed Cases
+            </Tooltip>
+          }>
+          <div className='Cases'>
+            <i className='fas fa-users'></i>
+            {'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+            {'222222222' > 0 && (
+              <span className='todayResults'>
+                +{'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              </span>
+            )}
+          </div>
+        </OverlayTrigger>
 
-export default WWStates
+        <OverlayTrigger
+          key='Deaths'
+          placement='bottom'
+          overlay={
+            <Tooltip className='myToolTip' id='tooltip-bottom'>
+              Confirmed Deaths
+            </Tooltip>
+          }>
+          <div className='Deaths'>
+            <i className='fas fa-skull'></i>
+            {'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+            {'222222222' > 0 && (
+              <span className='todayResults'>
+                +{'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              </span>
+            )}
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          key='Recovered'
+          placement='bottom'
+          overlay={
+            <Tooltip className='myToolTip' id='tooltip-bottom'>
+              Confirmed Recovered
+            </Tooltip>
+          }>
+          <div className='Recovered'>
+            <i className='fas fa-hospital-user'></i>
+            {'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+          </div>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          key='Tests'
+          placement='bottom'
+          overlay={
+            <Tooltip className='myToolTip' id='tooltip-bottom'>
+              Confirmed Tests
+            </Tooltip>
+          }>
+          <div className='Tests'>
+            <i className='fas fa-vial'></i>
+            {'222222222'.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+          </div>
+        </OverlayTrigger>
+      </div>
+    </div>
+  );
+};
+
+export default WWStates;
