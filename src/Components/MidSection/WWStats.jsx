b100890965f55ba3c83e60ce2106faa8fdbb3939
MedEBoudali <med.sneaky@gmail.com>
2020-04-23 22:35:54 +0100

    Now i can get current data to WWStates

--- src/Components/MidSection/WWStats.jsx
+++ src/Components/MidSection/WWStats.jsx
@@ -5,7 +5,10 @@ import Spinner from 'react-bootstrap/Spinner';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
-const WWStats = ({ AllCountriesData: { WWStats, allCountriesloading } }) => {
+const WWStats = ({
+  AllCountriesData: { WWStats, allCountriesloading },
+  CurrentCountryData: { currentCountry, currentCountryLoading }
+}) => {
   let {
     updated,
     cases,
@@ -26,6 +29,109 @@ const WWStats = ({ AllCountriesData: { WWStats, allCountriesloading } }) => {
             <span className='sr-only'>Loading...</span>
           </Spinner>
         </div>
+      ) : currentCountry !== null || currentCountryLoading ? (
+        <>
+          <h1 className='WWStatsHeader'>
+            <img src={currentCountry.flag} alt={currentCountry.country} />
+            {currentCountry.country}
+          </h1>
+          <div className='WCDR'>
+            <OverlayTrigger
+              key='Cases'
+              placement='bottom'
+              overlay={
+                <Tooltip className='myToolTip' id='tooltip-bottom'>
+                  Confirmed Cases
+                </Tooltip>
+              }>
+              <div className='WWCases'>
+                <p className='WWIcons'>
+                  <i className='fas fa-users'></i> Cases
+                </p>
+                {currentCountry.cases
+                  .toString()
+                  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+                {currentCountry.todayCases > 0 && (
+                  <span className='WWtodayResults'>
+                    +
+                    {currentCountry.todayCases
+                      .toString()
+                      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+                  </span>
+                )}
+              </div>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              key='Deaths'
+              placement='bottom'
+              overlay={
+                <Tooltip className='myToolTip' id='tooltip-bottom'>
+                  Confirmed Deaths
+                </Tooltip>
+              }>
+              <div className='WWDeaths'>
+                <p className='WWIcons'>
+                  <i className='fas fa-skull'></i> Deaths
+                </p>
+                {currentCountry.deaths
+                  .toString()
+                  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+                {currentCountry.todayDeaths > 0 && (
+                  <span className='WWtodayResults'>
+                    +
+                    {currentCountry.todayDeaths
+                      .toString()
+                      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+                  </span>
+                )}
+              </div>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              key='Recovered'
+              placement='bottom'
+              overlay={
+                <Tooltip className='myToolTip' id='tooltip-bottom'>
+                  Confirmed Recovered
+                </Tooltip>
+              }>
+              <div className='WWRecovered'>
+                <p className='WWIcons'>
+                  <i className='fas fa-hospital-user'></i> Recovered
+                </p>
+                {currentCountry.recovered
+                  .toString()
+                  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              </div>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              key='Tests'
+              placement='bottom'
+              overlay={
+                <Tooltip className='myToolTip' id='tooltip-bottom'>
+                  Confirmed Tests
+                </Tooltip>
+              }>
+              <div className='WWTests'>
+                <p className='WWIcons'>
+                  <i className='fas fa-vial'></i> Tests
+                </p>
+                {currentCountry.tests
+                  .toString()
+                  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              </div>
+            </OverlayTrigger>
+          </div>
+          <p className='lastUpdate'>
+            <i className='far fa-clock' />
+            Last update:
+            <span>
+              {currentCountry.updated > 0 && moment(updated).fromNow()}.
+            </span>
+          </p>
+        </>
       ) : (
         <>
           <h1 className='WWStatsHeader'>
@@ -124,7 +230,8 @@ const WWStats = ({ AllCountriesData: { WWStats, allCountriesloading } }) => {
 };
 
 const mapStateToProps = state => ({
-  AllCountriesData: state.AllCountries
+  AllCountriesData: state.AllCountries,
+  CurrentCountryData: state.CurrentCountryData
 });
 
 export default connect(mapStateToProps)(WWStats);
