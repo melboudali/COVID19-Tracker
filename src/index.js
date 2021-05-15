685766dd9d0e8cd7fa3d0ece0e12fa9fb51a9289
MedElBoudali <med.sneaky@gmail.com>
2021-05-15 22:29:44 +0000

    added service worker to index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './Reducers';
-import './Assets/Style/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./Reducers";
+import "./Assets/Style/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 ReactDOM.render(
-  <Provider
-    store={createStore(
-      rootReducer,
-      initialState,
-      composeWithDevTools(applyMiddleware(...middleware))
-    )}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
+
+serviceWorker.register();
